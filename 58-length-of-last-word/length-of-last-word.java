eff0c6582ff839274d5c02d1c2c9cf64e6dee575
Pranesh8556 <pranesh8556@gmail.com>
2025-12-23 14:39:30 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (24.22%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();                 // remove trailing spaces
+        int lastSpace = s.lastIndexOf(' ');
+        return s.length() - lastSpace - 1;
+    }
+}
